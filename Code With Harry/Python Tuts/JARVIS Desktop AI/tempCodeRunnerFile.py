86e33bf590ae88c69ff8d51a3bea28dd9e5381b4
Jay-Patil1 <69390522+Jay-Patil1@users.noreply.github.com>
2020-08-10 09:59:56 +0530

    Add files via upload

---
+++ Code With Harry/Python Tuts/JARVIS Desktop AI/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+except Exception as e:
+    #     # print(e)
+    #     print("Say that again please...")
+    #     return "None"
+    # return query
---
+++ Code With Harry/Python Tuts/JARVIS Desktop AI/trial.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+webbrowser.open('https://www.youtube.com/')
+webbrowser.open("https://www.google.com/")
+webbrowser.open("https://www.stackoverflow.com/")
+webbrowser.open('https://wikipedia.com/')
